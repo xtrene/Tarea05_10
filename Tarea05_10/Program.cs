e8de876e8b6fb81498bd3095b0cbc07e026b75ad
Carlos <Carlos@xtrene.com>
2021-06-25 19:04:32 +0200

    Agregar archivos de proyecto.

---
+++ Tarea05_10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tarea05_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 5;
+            int[] tabla = new int[11];
+            Console.WriteLine("Tabla del {0}: ", numero);
+            for (int i=0; i < tabla.Length; i++)
+            {
+                tabla[i] = numero * i;
+                Console.WriteLine("{0} x {1} = {2}", numero, i, tabla[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
